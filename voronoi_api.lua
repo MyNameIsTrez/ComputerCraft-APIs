f0be02ef10fbdc385697ea66d27b64a5ec62f948
MyNameIsTrez <welfje@gmail.com>
2020-05-10 02:46:18 +0200

    Optimized the API to be 3.5x faster.

--- voronoi_api.lua
+++ voronoi_api.lua
@@ -1,20 +1,25 @@
+-- Class table
 Voronoi = {}
 
 
+-- Constructor
 function Voronoi:new(pointCount)
 	setmetatable({}, Voronoi)
 	self.pointCount = pointCount
 	self.width, self.height = term.getSize()
+	self.width = self.width - 1 -- Compensates for adding the invisible '\n' char at the end of every row.
+	self.chars = { ".", "'", ":", "-", "!", "/", "(", "=", "%", "1", "C", "3", "$", "2", "5", "A", "0", "#", "@" }
 	return self
 end
 
 
+-- Methods
 function Voronoi:createPoints()
 	self.points = {}
 	for i = 1, self.pointCount do
 		local point = {
-			pos = vector.new(math.random(self.width - 1), math.random(self.height)),
-			char = dithering.getClosestChar(i / self.pointCount)
+			pos = vector.new(math.random(self.width), math.random(self.height)),
+			char = self.chars[(i - 1) % #self.chars + 1] -- Cycles through and reuses the available chars.
 		}
 		table.insert(self.points, point)
 	end
@@ -23,25 +28,27 @@ end
 function Voronoi:drawPixels()
 	local charTable = {}
 	for y = 1, self.height do
-		for x = 1, self.width - 1 do
-			local distancesData = {}
+		for x = 1, self.width do
+			local smallestDist = 1/0 -- 1/0 is 'inf'; used because 'inf' directly is not recognized.
+			local char
 			for i = 1, self.pointCount do
 				local point = self.points[i]
 				local xDiff = x - point.pos.x
 				local yDiff = y - point.pos.y
-				local distSq = xDiff^2 + yDiff^2
-				table.insert(distancesData, { distSq = distSq, char = point.char })
+				local distSq = xDiff^2 + yDiff^2 -- Don't need sqrt when comparing.
+				if distSq < smallestDist then
+					smallestDist = distSq
+					char = point.char
+				end
 			end
-			-- Sort ascending.
-			table.sort(distancesData, function(a, b) return a.distSq < b.distSq end)
 			term.setCursorPos(x, y)
-			local char = distancesData[1].char -- Lua tables start at 1.
 			table.insert(charTable, char)
 		end
 		if y ~= self.height then table.insert(charTable, "\n") end
 		cf.tryYield() -- Circumvents default crash after 5s.
 	end
 	term.setCursorPos(1, 1)
-	local str = table.concat(charTable)
+	local str = table.concat(charTable) -- Concatenates all characters into a single string.
 	write(str)
+	term.setCursorPos(1, 1) -- Can be edited if the user wants to end elsewhere.
 end
