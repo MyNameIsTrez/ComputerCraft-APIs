41f6f21d718c9833fd1f24801f6eb8005424a784
MyNameIsTrez <welfje@gmail.com>
2021-03-21 01:49:58 +0100

    Almost working.

--- files/api_manager.lua
+++ files/api_manager.lua
@@ -1,25 +1,53 @@
 -- ComputerCraft and server code written and hosted by MyNameIsTrez#1585 on Discord. Feel free to contact me!
 
 function get_latest()
-	local url = "http://h2896147.stratoserver.net:1338/apis-get-latest"
+	fs.makeDir("apis")
 	
 	if not json then download_json() end
-	local object_string = "data=" .. json.encode(get_local_api_versions())
-	print("object_string: " .. object_string)
+	local serialized_api_metadata = "data=" .. json.encode(get_local_api_versions())
+	print("serialized_api_metadata: " .. serialized_api_metadata)
 	
-	local outdated = http.post(url, object_string)
+	local url = "http://h2896147.stratoserver.net:1338/apis-get-latest"
+	local h = http.post(url, serialized_api_metadata)
+	local diff_msg = h.readAll()
 	
-	return outdated
+	h = io.open("diff_msg.json", "w") h:write(diff_msg) h:close()
+	local diff_apis = json.decode(diff_msg)
+	
+	local key_count = 0
+	for _, _ in pairs(diff_apis) do key_count = key_count + 1 end
+	print("key count diff_apis:", ", ", key_count)
+
+	for k, v in pairs(diff_apis) do
+		h = io.open(fs.combine("apis", k), "w")
+		h:write(diff_apis[k].lua)
+		h:close()
+		
+		--cf.printTable(diff_apis[k].lua)
+
+		diff_apis[k].lua = nil -- So code doesn't end up in metadata file.
+		
+		print("Downloaded " .. k .. ": " .. diff_apis[k].age)
+	end
+
+	h = io.open("apis/api_metadata.json", "w")
+	h:write(textutils.serialize(diff_apis))
+	h:close()
 end
 
 function get_local_api_versions()
-	-- TODO: Read from api_metadata.json
-	return {foo=8, bar=123}
+	if fs.exists("apis/api_metadata.json") then
+		local h = io.open("apis/api_metadata.json", "r")
+		local data = h:read()
+		h:close()
+		return textutils.unserialize(data)
+	else
+		return {}
+	end
 end
 
 -- TODO: Move to BackwardsOS bootloader.
 function download_json()
-	fs.makeDir("apis")
 	local h = io.open("apis/json", "w")
 	h:write(https.get("https://raw.githubusercontent.com/MyNameIsTrez/ComputerCraft-APIs/master/apis/json.lua").readAll())
 	h:close()
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
 
 const app = express();
@@ -27,8 +29,68 @@ app.get("/api-manager-dl", (req, res) => {
 });
 
 
+// TODO: Refactor into subfunctions.
 app.post("/apis-get-latest", (httpRequest, httpResponse) => {
 	printStats("apis-get-latest");
-	console.log(JSON.parse(httpRequest.body.data))
-	httpResponse.end();
+
+	console.log("httpRequest.body.data:");
+	console.log(httpRequest.body.data);
+
+	const data = httpRequest.body.data;
+	let msgString;
+	if (data == "[]") {
+		msgString = data;
+	} else {
+		//msgString = data.slice(1, -1).replace(/\\/g, "");
+		msgString = data.replace(/\\/g, "");
+	}
+	console.log("msgString:", msgString);
+	const userAPIs = JSON.parse(msgString);
+	
+	//console.log(typeof(userAPIs));
+	//console.log(userAPIs["matrix"]);
+	
+	console.log("userAPIs key length:", Object.keys(userAPIs).length);
+	
+	console.log("userAPIs:")
+	console.log(userAPIs);
+
+	let diffAPIs = {};
+	
+	fs.readdir("apis", (err, serverAPINames) => {
+		const serverAPIsData = {};
+		serverAPINames.forEach(serverAPIBase => { // Base means name + extension, so foo.lua
+			const APIPath = path.join("apis", serverAPIBase);
+			const stats = fs.statSync(APIPath);
+			
+			const lua_code = fs.readFileSync(APIPath, "utf8");
+			
+			const serverAPIName = path.parse(serverAPIBase).name; // Trims .lua
+			
+			serverAPIsData[serverAPIName] = { "age": stats.mtime, "lua": lua_code};
+		});
+
+		// If the user doesn't have any APIs yet, just send all of the server's API data.
+		if (Array.isArray(userAPIs) && userAPIs.length === 0) {
+			diffAPIs = serverAPIsData;
+		} else {
+	  		for (const [serverAPIName, serverAPIData] of Object.entries(serverAPIsData)) {
+				// Age is Unix time; a newer file has a larger Unix time for its modification date.
+				let userAPIAge;
+				if (userAPIs.hasOwnProperty(serverAPIName)) {
+					userAPIAge = userAPIs[serverAPIName].age;
+				}
+				
+				const serverAPIAge = serverAPIData.age;
+	
+				if (userAPIAge === undefined || serverAPIAge > userAPIAge) {
+					diffAPIs[serverAPIName] = serverAPIData;
+				}
+	  		}
+		}
+
+		console.log("diffAPIs key count:", Object.keys(diffAPIs).length);
+		console.log("diffAPIs type:", diffAPIs);
+		httpResponse.send(diffAPIs);
+	});
 });
