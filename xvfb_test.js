675c755ab86985588d547ce34151abb7bcd005bb
MyNameIsTrez <welfje@gmail.com>
2021-04-16 19:58:34 +0200

    Added back the Python program that generates items.json, as it makes more sense to run the Python program manually than to have JS check if it needs to initialize items.json.

--- xvfb_test.js
+++ xvfb_test.js
@@ -1,6 +1,7 @@
 var Xvfb = require('xvfb');
 var xvfb = new Xvfb();
 xvfb.start(function(err, xvfbProcess) {
+  // TODO: How am I supposed to use the virtual frame buffer though??:
   // code that uses the virtual frame buffer here
   console.log(err);
   console.log(xvfbProcess);
